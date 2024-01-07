83ca53dafb6c748fafb1af64b29068fa1db8f5b2
chelmerd <chelmerd@student.42wolfsburg.de>
2024-01-07 14:58:34 +0000

    add 2015 day 5 part 1

---
+++ 2015/05_doesnt_he_have_intern-elves_for_this/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+
+	"github.com/SuperDulli/advent-of-code/util"
+)
+
+func main() {
+	var part int
+	var inputFileName string
+	flag.IntVar(&part, "part", 1, "part 1 or 2")
+	flag.StringVar(&inputFileName, "file", "input.txt", "alternate input file")
+	flag.Parse()
+	fmt.Println("Running part", part)
+
+	input := util.ReadLines(inputFileName)
+
+	if part == 1 {
+		ans := part1(input)
+		fmt.Println("Output:", ans)
+	} else {
+		ans := part2(input)
+		fmt.Println("Output:", ans)
+	}
+}
+
+func part1(input []string) int {
+	count := 0
+	for _, line := range input {
+		if isNice(line) {
+			count++
+		}
+	}
+	return count
+}
+
+func part2(input []string) int {
+	return 0
+}
+
+func isNice(s string) bool {
+	vowelCount := 0
+	hasRepetition := false
+	bannedSeq := map[string]int{
+		"ab": 1,
+		"cd": 1,
+		"pq": 1,
+		"xy": 1,
+	}
+	previous := ' '
+	for n, c := range s {
+		if vowelCount < 3 && strings.ContainsRune("aeiou", c) {
+			vowelCount++
+		}
+		if n == 0 {
+			previous = c
+			continue
+		}
+		substr := string(previous) + string(c)
+		if !hasRepetition && c == previous {
+			hasRepetition = true
+		}
+		if _, ok := bannedSeq[substr]; ok {
+			return false
+		}
+		previous = c
+	}
+	return vowelCount >= 3 && hasRepetition
+}
