e5d900eab45ef72ec9e3a4621ef98ddf7729e800
chelmerd <chelmerd@student.42wolfsburg.de>
2024-10-11 14:29:53 +0000

    add 2020 day 1 part 1

---
+++ 2020/01_report_repair/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/SuperDulli/advent-of-code/util"
+)
+
+func main() {
+	var part int
+	var inputFileName string
+	flag.IntVar(&part, "part", 1, "part 1 or 2")
+	flag.StringVar(&inputFileName, "file", "input.txt", "alternate input file")
+	flag.Parse()
+	fmt.Println("Running part", part)
+
+	input := util.ReadLines(inputFileName)
+
+	if part == 1 {
+		ans := part1(input)
+		fmt.Println("Output:", ans)
+	} else {
+		ans := part2(input)
+		fmt.Println("Output:", ans)
+	}
+}
+
+func part1(input []string) int {
+	numbers := util.ConvertToNumbers(input)
+	for i, n := range numbers {
+		for j := i + 1; j < len(numbers); j++ {
+			m := numbers[j]
+			result := n + m
+			if result == 2020 {
+				return n * m
+			}
+		}
+	}
+	return 0
+}
+
+func part2(input []string) int {
+	return 0
+}
